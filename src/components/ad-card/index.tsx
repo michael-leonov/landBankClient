7b4cf775b5bdc8e341ef3e8cc6bb2a0e52982282
Mikhail <michael.leonov96@mail.ru>
2023-07-24 15:24:08 +0300

    add no photos announcement handling

--- src/components/ad-card/index.tsx
+++ src/components/ad-card/index.tsx
@@ -4,41 +4,26 @@ import * as S from './styles';
 import { getPriceWithSpaces } from '../../utils/getPriceWithSpaces';
 import { useNavigate } from 'react-router-dom';
 import { ADS_ROUTE } from '../../utils/consts';
+import noImageAvailable from '../../assets/no-image.png';
 
 const AdCard = ({ id, title, address, price, description, url, photos }: AdProps) => {
-  // const getFoundedImage = (arrImgStrings: string[]): string => {
-  //   const img = new Image();
-  //   const BreakError = {};
-
-  //   try {
-  //     arrImgStrings.forEach((imgString: string) => {
-  //       // img.onerror = () => {
-  //       //   return;
-  //       // };
-
-  //       img.onload = () => {
-  //         return (img.src = imgString);
-  //       };
-  //     });
-  //   } catch (err) {
-  //     if (err !== BreakError) throw err;
-  //   }
-
-  //   return img.src;
-  // };
-
   const navigate = useNavigate();
 
   const goToAdPageOnClickHandler = (id: number): void => {
     navigate(`${ADS_ROUTE}/${id}`);
   };
 
+  const onErrorImageHandler = (currentTarget: EventTarget & HTMLImageElement): void => {
+    currentTarget.onerror = null;
+    currentTarget.src = noImageAvailable;
+  };
+
   return (
     <S.CardWrapper onClick={() => goToAdPageOnClickHandler(id)}>
       <S.CardImg
-        // src={getFoundedImage(photos)}
-        src={photos[0]}
+        src={photos[0] || noImageAvailable}
         alt={title}
+        onError={({ currentTarget }) => onErrorImageHandler(currentTarget)}
       />
 
       <S.CardInfo>
--- src/components/ad-photos-block/index.tsx
+++ src/components/ad-photos-block/index.tsx
@@ -1,19 +1,38 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react';
 import * as S from './styles';
+import noImageAvailable from '../../assets/no-image.png';
 
 const AdPhotosBlock = ({ photos, title, activeImg, setActiveImg }: any) => {
+  const onErrorImageHandler = (currentTarget: EventTarget & HTMLImageElement): void => {
+    currentTarget.onerror = null;
+    currentTarget.style.display = 'none';
+    const imgWrapper = currentTarget.parentElement as HTMLDivElement;
+    imgWrapper.style.display = 'none';
+  };
+
   return (
     <S.AdPhotosBlock>
       <S.CurrentAdvImageWrapper>
-        <S.CurrentAdvImage src={photos[activeImg]} alt={title} />
+        <S.CurrentAdvImage
+          src={photos[activeImg]}
+          alt={title}
+          onError={({ currentTarget }) => {
+            currentTarget.onerror = null;
+            currentTarget.src = noImageAvailable;
+          }}
+        />
       </S.CurrentAdvImageWrapper>
 
       {photos.length > 1 && (
         <S.AdvImagesList>
           {photos.map((photo: string, i: number) => (
             <S.AdvImageWrapper key={photo} onClick={() => setActiveImg(i)} active={i === activeImg}>
-              <S.AdvImage src={photo} alt={title} />
+              <S.AdvImage
+                src={photo}
+                alt={title}
+                onError={({ currentTarget }) => onErrorImageHandler(currentTarget)}
+              />
             </S.AdvImageWrapper>
           ))}
         </S.AdvImagesList>
