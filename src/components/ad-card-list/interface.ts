748702f440ff4b24cdf4f7fb1ae774627b4f77cc
Mikhail <michael.leonov96@mail.ru>
2023-07-25 09:55:07 +0300

    add error handling for ads

---
+++ src/components/ad-card-list/interface.ts
@@ -0,0 +1,14 @@
+import { SerializedError } from '@reduxjs/toolkit';
+import { FetchBaseQueryError } from '@reduxjs/toolkit/dist/query';
+
+import { Ad } from '../../redux/services/ads/interface';
+
+interface AdListProps {
+  ads?: Ad[];
+  error?: FetchBaseQueryError | SerializedError;
+  isError: boolean;
+  isLoading: boolean;
+  isSuccess: boolean;
+}
+
+export default AdListProps;
