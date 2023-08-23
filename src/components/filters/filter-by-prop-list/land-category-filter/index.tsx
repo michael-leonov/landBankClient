b030e1928533391057f2e2a8ccbeb956892b824f
Mikhail <michael.leonov96@mail.ru>
2023-08-23 13:28:55 +0300

    feat: add land category filter

---
+++ src/components/filters/filter-by-prop-list/land-category-filter/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { landCategories } from './options';
+import * as S from './styles';
+import LandCategoryFilterTypeProp from './types';
+
+const LandCategoryFilter = ({ register }: LandCategoryFilterTypeProp) => {
+  return (
+    <S.LandCategoryInputsBlock>
+      {landCategories.map((category) => (
+        <S.LandCategoryInputWrapper key={category.value}>
+          <S.LandCategoryInputLabel>
+            <input
+              type='checkbox'
+              value={category.value}
+              {...register('landCategory', {
+                required: false,
+              })}
+            />
+            <span>{category.title}</span>
+          </S.LandCategoryInputLabel>
+        </S.LandCategoryInputWrapper>
+      ))}
+    </S.LandCategoryInputsBlock>
+  );
+};
+
+export default LandCategoryFilter;
---
+++ src/components/filters/filter-by-prop-list/land-category-filter/types.ts
@@ -0,0 +1,5 @@
+import FiltersByPropListProps from '../interface';
+
+type LandCategoryFilterTypeProp = Pick<FiltersByPropListProps, 'register'>;
+
+export default LandCategoryFilterTypeProp;
