27c8641e6ccca9e5748fa012ffc16de9f3d48b88
Mikhail <michael.leonov96@mail.ru>
2023-08-23 13:27:58 +0300

    feat: add area unit filter

---
+++ src/components/filters/filter-by-prop-list/area-unit-filter/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const SelectAreaUnit = styled.select`
+  margin-bottom: 8px;
+  padding: 4px 6px;
+  width: 30%;
+  border: 1px solid #b9c2c8;
+  border-radius: 5px;
+`;
