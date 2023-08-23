3905711667a6e4b1df07883d14fef1fb49346b6b
Mikhail <michael.leonov96@mail.ru>
2023-08-23 13:29:27 +0300

    refactor: update price filter

---
+++ src/components/filters/filter-by-prop-list/price-filter/styles.ts
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+import { device } from '../../../../utils/consts';
+
+export const PriceInputsWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  border: 1px solid #b9c2c8;
+  border-radius: 5px;
+  overflow: hidden;
+
+  @media ${device.laptop} {
+    justify-content: space-between;
+  }
+
+  div {
+    width: 30px;
+    height: 1px;
+    background: #979797;
+
+    @media ${device.laptop} {
+      width: 10px;
+    }
+  }
+`;
+
+export const PriceInput = styled.input`
+  border: none;
+  padding: 10px 10px;
+  width: calc((100% - 30px) / 2);
+
+  &:first-child {
+    padding-right: 5px;
+  }
+
+  &:last-child {
+    padding-left: 5px;
+    text-align: right;
+  }
+`;
---
+++ src/components/filters/filter-by-prop-list/price-filter/types.ts
@@ -0,0 +1,5 @@
+import FiltersByPropListProps from '../interface';
+
+type PriceFilterTypeProp = Pick<FiltersByPropListProps, 'register'>;
+
+export default PriceFilterTypeProp;
