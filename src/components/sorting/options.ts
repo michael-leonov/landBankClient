bef438a1d493f6e1b6aa34601e36ed8976585ac1
Mikhail <michael.leonov96@mail.ru>
2023-08-23 14:17:55 +0300

    feat: add sorting

---
+++ src/components/sorting/options.ts
@@ -0,0 +1,8 @@
+export const sorting = [
+  { order: 'DESC', title: 'По умолчанию', value: 'id' },
+  { order: 'ASC', title: 'Дешевле', value: 'price' },
+  { order: 'DESC', title: 'Дороже', value: 'price' },
+  // { title: 'Сначала новые', value: 'new' },
+  { order: 'ASC', title: 'Мин. площадь', value: 'area' },
+  { order: 'DESC', title: 'Макс. площадь', value: 'area' },
+];
