83a8467b327836c013fcd5dead4f14ff7d1caf18
Mikhail <michael.leonov96@mail.ru>
2023-08-23 13:28:30 +0300

    feat: add date published filter

---
+++ src/components/filters/filter-by-prop-list/date-published-filter/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const DatePublishedInputsBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 10px;
+`;
+
+export const DatePublishedInputWrapper = styled.div`
+  display: flex;
+  column-gap: 10px;
+`;
+
+export const DatePublishedInputLabel = styled.label`
+  display: flex;
+  column-gap: 10px;
+`;
---
+++ src/components/filters/filter-by-prop-list/date-published-filter/types.ts
@@ -0,0 +1,5 @@
+import FiltersByPropListProps from '../interface';
+
+type DatePublishedFilterTypeProp = Pick<FiltersByPropListProps, 'register'>;
+
+export default DatePublishedFilterTypeProp;
