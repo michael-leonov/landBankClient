7d3d3fd22c7aecf54f77dccf2bc3be8bd1af1aac
Mikhail <michael.leonov96@mail.ru>
2023-08-23 13:29:48 +0300

    feat: add rent filter

---
+++ src/components/filters/filter-by-prop-list/rent-filter/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { rents } from './options';
+import * as S from './styles';
+import RentFilterTypeProp from './types';
+
+const RentFilter = ({ register }: RentFilterTypeProp) => {
+  return (
+    <S.RentInputsBlock>
+      {rents.map((rent) => (
+        <S.RentInputWrapper key={rent.title}>
+          <S.RentInputLabel>
+            <input
+              type='radio'
+              value={rent.value}
+              {...register('isRent', {
+                required: false,
+              })}
+            />
+            <span>{rent.title}</span>
+          </S.RentInputLabel>
+        </S.RentInputWrapper>
+      ))}
+    </S.RentInputsBlock>
+  );
+};
+
+export default RentFilter;
