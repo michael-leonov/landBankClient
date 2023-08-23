c032778dd317dcc948459e1656a04c7ca87062fb
Mikhail <michael.leonov96@mail.ru>
2023-08-23 13:33:18 +0300

    refactor: update search bar

--- src/components/filters/search-bar/styles.ts
+++ src/components/filters/search-bar/styles.ts
@@ -1,28 +1,13 @@
 import styled from 'styled-components';
 
-import { device } from '../../../utils/consts';
-
 export const SearchBarWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  row-gap: 4px;
-  background-color: #fff;
-  padding: 4px 8px;
-  flex: 1 1;
-  margin-right: 6px;
-
-  @media ${device.laptop} {
-    border-right: 1px solid #f2f4f6;
-    margin-right: 0;
-  }
+  border: 1px solid #b9c2c8;
+  border-radius: 5px;
+  overflow: hidden;
 `;
 
-export const SearchBarLabel = styled.label`
-  font-size: 10px;
-`;
-
-export const SearchBarInput = styled.input`
+export const SearchBar = styled.input`
   border: none;
-  height: 100%;
+  padding: 10px 10px;
   width: 100%;
 `;
