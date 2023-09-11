d9bffdfa9756b71754ff6531714089d3b07ef836
Mikhail <michael.leonov96@mail.ru>
2023-09-11 17:16:09 +0300

    feat: add announcement removal functionality

--- src/components/ad-details/index.tsx
+++ src/components/ad-details/index.tsx
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
 import { useCookies } from 'react-cookie';
-import { Link, To } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import checkedAdIcon from '../../assets/checked-ad-icon.png';
 import { useAppSelector } from '../../redux/hooks';
-import { Ad } from '../../redux/services/ads/interface';
 import { selectUser } from '../../redux/slices/userSlice';
 import { Role } from '../../redux/slices/userSlice/interface';
 import { StyledContainer } from '../../styles/common-styled-components/styles';
@@ -39,82 +38,86 @@ const AdDetails = ({ ad }: AdDetailsProps) => {
   const [cookies] = useCookies(['token']);
 
   return (
-    <S.AdDetailsBlock>
-      <StyledContainer>
-        <S.ShortInfoBlock>
-          <AdPhotosBlock
-            photos={ad?.photos || []}
-            title={ad?.title}
-            activeImg={activeImg}
-            setActiveImg={setActiveImg}
-            isBankZemel={isBankZemel}
-          />
-          <S.ShortInfoWrapper>
-            <S.AdTitleAndPriceWrapper>
-              <S.TitleWrapper>
-                <S.Title>{ad?.title}</S.Title>
-                {ad?.isChecked && (
-                  <S.AdCheckedIcon
-                    src={checkedAdIcon}
-                    title='Проверено модератором'
-                    isChecked={ad?.isChecked}
-                  />
+    <>
+      {ad && (
+        <S.AdDetailsBlock>
+          <StyledContainer>
+            <S.ShortInfoBlock>
+              <AdPhotosBlock
+                photos={ad.photos || []}
+                title={ad.title}
+                activeImg={activeImg}
+                setActiveImg={setActiveImg}
+                isBankZemel={isBankZemel}
+              />
+              <S.ShortInfoWrapper>
+                <S.AdTitleAndPriceWrapper>
+                  <S.TitleWrapper>
+                    <S.Title>{ad.title}</S.Title>
+                    {ad.isChecked && (
+                      <S.AdCheckedIcon
+                        src={checkedAdIcon}
+                        title='Проверено модератором'
+                        isChecked={ad.isChecked}
+                      />
+                    )}
+                  </S.TitleWrapper>
+
+                  <S.Price>{getPriceWithSpaces(ad.price.toString())} ₽</S.Price>
+                </S.AdTitleAndPriceWrapper>
+                {ad.date_published && (
+                  <S.DatePublished>Опубликовано: {ad.date_published}</S.DatePublished>
                 )}
-              </S.TitleWrapper>
 
-              <S.Price>{getPriceWithSpaces(ad?.price.toString())} ₽</S.Price>
-            </S.AdTitleAndPriceWrapper>
-            {ad?.date_published && (
-              <S.DatePublished>Опубликовано: {ad?.date_published}</S.DatePublished>
-            )}
+                <S.Adress>{ad.address}</S.Adress>
+                <S.AdsEditorBtnsWrapper>
+                  <AddToFavoritesBtn />
+                  {isAdsEditor && (
+                    <>
+                      <ToggleCheckedAdBtn ad={ad} token={cookies?.token} />
+                      <AddCommentToAdBtn />
+                      <EditAdBtn />
+                      <RemoveAdBtn announcementId={ad.id} />
+                    </>
+                  )}
+                </S.AdsEditorBtnsWrapper>
+              </S.ShortInfoWrapper>
+            </S.ShortInfoBlock>
+          </StyledContainer>
 
-            <S.Adress>{ad?.address}</S.Adress>
-            <S.AdsEditorBtnsWrapper>
-              <AddToFavoritesBtn />
-              {isAdsEditor && (
-                <>
-                  <ToggleCheckedAdBtn ad={ad} token={cookies?.token} />
-                  <AddCommentToAdBtn />
-                  <EditAdBtn />
-                  <RemoveAdBtn />
-                </>
-              )}
-            </S.AdsEditorBtnsWrapper>
-          </S.ShortInfoWrapper>
-        </S.ShortInfoBlock>
-      </StyledContainer>
+          <S.MobSliderWrapper>
+            <AdSliderPhotos
+              photos={ad.photos || []}
+              title={ad.title}
+              isSuccess={true}
+              isBankZemel={isBankZemel}
+            />
+          </S.MobSliderWrapper>
 
-      <S.MobSliderWrapper>
-        <AdSliderPhotos
-          photos={ad?.photos || []}
-          title={ad?.title}
-          isSuccess={true}
-          isBankZemel={isBankZemel}
-        />
-      </S.MobSliderWrapper>
+          <StyledContainer>
+            <h2>Описание</h2>
+            <S.Description>{ad.description}</S.Description>
+            <S.SourceLinkWrapper>
+              <Link to={ad.url} target='_blank'>
+                Источник
+              </Link>
+            </S.SourceLinkWrapper>
 
-      <StyledContainer>
-        <h2>Описание</h2>
-        <S.Description>{ad?.description}</S.Description>
-        <S.SourceLinkWrapper>
-          <Link to={ad?.url as To} target='_blank'>
-            Источник
-          </Link>
-        </S.SourceLinkWrapper>
-
-        <S.BtnWrapper>
-          <CustomButton
-            type='button'
-            onClick={() => setIsShowMap(!isShowMap)}
-            disabled={false}
-            variant='outlined'
-          >
-            {isShowMap ? 'Скрыть' : 'Посмотреть на карте'}
-          </CustomButton>
-        </S.BtnWrapper>
-        {isShowMap && <AdsMap ads={[ad] as Ad[]} defaultLat={ad?.lat} defaultLon={ad?.lon} />}
-      </StyledContainer>
-    </S.AdDetailsBlock>
+            <S.BtnWrapper>
+              <CustomButton
+                type='button'
+                onClick={() => setIsShowMap(!isShowMap)}
+                disabled={false}
+                variant='outlined'
+              >
+                {isShowMap ? 'Скрыть' : 'Посмотреть на карте'}
+              </CustomButton>
+            </S.BtnWrapper>
+            {isShowMap && <AdsMap ads={[ad]} defaultLat={ad.lat} defaultLon={ad.lon} />}
+          </StyledContainer>
+        </S.AdDetailsBlock>
+      )}
+    </>
   );
 };
 
--- src/components/remove-ad-btn/index.tsx
+++ src/components/remove-ad-btn/index.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import { useCookies } from 'react-cookie';
+import { useNavigate } from 'react-router-dom';
 
+import { useRemoveAdMutation } from '../../redux/services/ads/adsApi';
+import { ADS_ROUTE } from '../../utils/consts';
+import loadingTextBtn from '../../utils/funcs/loadingTextBtn';
 import CustomButton from '../custom-button';
 
-const RemoveAdBtn = () => {
+const RemoveAdBtn = ({ announcementId }: { announcementId: number }) => {
+  const [removeAd, { isLoading }] = useRemoveAdMutation();
+  const [cookies] = useCookies(['token']);
+  const navigate = useNavigate();
+
+  const removeAdHandler = async (): Promise<void> => {
+    await removeAd({ id: announcementId, token: cookies.token })
+      .unwrap()
+      .then(() => {
+        navigate(ADS_ROUTE);
+      });
+  };
+
   return (
-    <CustomButton type='button' disabled={false} variant='outlined'>
-      Удалить объявление
+    <CustomButton type='button' disabled={isLoading} variant='outlined' onClick={removeAdHandler}>
+      {isLoading ? loadingTextBtn : 'Удалить объявление'}
     </CustomButton>
   );
 };
