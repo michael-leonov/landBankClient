cffaa108a48c33eb4c543369d9ee46f429438c04
Mikhail <michael.leonov96@mail.ru>
2023-09-20 10:23:09 +0300

    feat: redirect authorized user to profile page

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import AuthForm from '../../components/auth-form';
+import { useAppSelector } from '../../redux/hooks';
+import { selectUser } from '../../redux/slices/userSlice';
 import { StyledContainer, StyledSection } from '../../styles/common-styled-components/styles';
 import { LOGIN_ROUTE } from '../../utils/consts';
 import * as S from './styles';
@@ -10,6 +12,16 @@ const Auth = () => {
   const { pathname } = useLocation();
 
   const isLogin = pathname === LOGIN_ROUTE;
+
+  const { isAuth, userInfo } = useAppSelector(selectUser);
+
+  const navigate = useNavigate();
+
+  if (isAuth) {
+    if (userInfo) {
+      navigate(`/profile/${userInfo.id}`);
+    }
+  }
 
   return (
     <StyledSection>
