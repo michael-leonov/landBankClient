41fb122f3cc508520a95eb4b2b2e097a2315db80
Mikhail <michael.leonov96@mail.ru>
2023-09-27 09:19:05 +0300

    refactor: change download ads

--- src/components/download-ads-xlsx/interface.ts
+++ src/components/download-ads-xlsx/interface.ts
@@ -4,6 +4,7 @@ interface DownloadAdsXlsxProps {
   isLoading: boolean;
   isSuccess: boolean;
   listAnnouncement: Ad[] | undefined;
+  page: number | string;
 }
 
 export default DownloadAdsXlsxProps;
