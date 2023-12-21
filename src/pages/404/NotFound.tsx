6e91dbe21f2281f39f578768b90449a87a0a4ca8
Mikhail <michael.leonov96@mail.ru>
2023-12-21 09:48:11 +0300

    feat: add 404 page

--- src/pages/404/NotFound.tsx
+++ src/pages/404/NotFound.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
+import { HOME_ROUTE } from '../../utils/consts';
+import * as S from './styles';
+
 const NotFound = () => {
-  return <div>404 page</div>;
+  return (
+    <S.NotFoundBlock>
+      <S.NotFoundWrapper>
+        <S.NotFoundTitleWrapper>
+          <S.NotFoundTitle>404</S.NotFoundTitle>
+        </S.NotFoundTitleWrapper>
+        <S.NotFoundSubtitle>Что-то пошло не так...</S.NotFoundSubtitle>
+        <S.NotFoundDescription>
+          Страница, которую вы ищете, могла быть удалена из-за изменения ее названия или временно
+          недоступна. <S.ReturnHomeLink to={HOME_ROUTE}>Вернуться на главную</S.ReturnHomeLink>
+        </S.NotFoundDescription>
+      </S.NotFoundWrapper>
+    </S.NotFoundBlock>
+  );
 };
 
 export default NotFound;
---
+++ src/pages/404/styles.ts
@@ -0,0 +1,79 @@
+import { Link } from 'react-router-dom';
+
+import styled from 'styled-components';
+
+export const NotFoundBlock = styled.div`
+  position: relative;
+  height: 100vh;
+`;
+
+export const NotFoundWrapper = styled.div`
+  line-height: 1.4;
+  text-align: center;
+  max-width: 767px;
+  width: 100%;
+  padding: 15px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+`;
+
+export const NotFoundTitleWrapper = styled.div`
+  position: relative;
+  height: 168px;
+
+  @media only screen and (min-width: 480px) {
+    height: 220px;
+  }
+`;
+
+export const NotFoundTitle = styled.h1`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  font-size: 142px;
+  font-weight: 200;
+  margin: 0;
+  background: linear-gradient(130deg, #545454, #81e383);
+  color: transparent;
+  -webkit-background-clip: text;
+  background-clip: text;
+  text-transform: uppercase;
+
+  @media only screen and (min-width: 480px) {
+    font-size: 186px;
+  }
+`;
+
+export const NotFoundSubtitle = styled.h2`
+  font-size: 22px;
+  font-weight: 200;
+  text-transform: uppercase;
+  margin-top: 0;
+  margin-bottom: 25px;
+  letter-spacing: 3px;
+
+  @media only screen and (min-width: 480px) {
+    font-size: 33px;
+  }
+`;
+
+export const NotFoundDescription = styled.p`
+  font-size: 16px;
+  font-weight: 200;
+  margin-top: 0;
+  margin-bottom: 25px;
+`;
+
+export const ReturnHomeLink = styled(Link)`
+  font-weight: 200;
+  text-decoration: none;
+  border-bottom: 1px dashed #545454;
+  border-radius: 2px;
+`;
